90da97b6c5974f6031fbe28b54f2843848115af5
Pantawadee <65309010008@chontech.ac.th>
2023-01-12 11:59:05 +0700

    province

--- conn.php
+++ conn.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "it65309010002";
-$password = "it12345";
-$dbname = "it65309010002";
+$username = "root";
+$password = "12345678";
+$dbname = "web2111";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_charset($conn,"utf8");
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
