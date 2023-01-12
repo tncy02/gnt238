4088f4cae57fb7713f719c47a19e3fed7dbdfb0d
tncy02 <65309010002@chontech.ac.th>
2023-01-12 16:06:09 +0700

    banner-edit

--- banner.php
+++ banner.php
@@ -1,5 +1,5 @@
-<div class="p-5 bg-secondary text-white text-center" style="background-image: ;background-size:cover">
-  <h1 class="text-dark"> D A Y - 1 0 </h1>
+<div class="p-5 bg-danger text-center " style="background-image: linear-gradient(120deg, #DEA4FF, #FFA4A4, #A4FFF8)">
+  <h1 class="text-white" style="font-family:cursive"> D A Y - 1 0 </h1>
   <p></p> 
 </div>
 
