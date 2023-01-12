98dd932f196d65adc94a7a94075386a1f136fd1a
tncy02 <65309010002@chontech.ac.th>
2023-01-12 11:44:07 +0700

    customer

---
+++ showCustomer.php
@@ -0,0 +1,45 @@
+<?php
+    include "header.php";
+    include "banner.php";
+    include "navbar.php";
+    include "conn.php";
+?>
+
+<div class="container mt-5">
+    <table class="table table-dark table-hover table table-striped table-bordered table-hover table-success" style="font-family:cursive;">
+        <tr>
+        <th>Customer ID</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Position</th>
+        <th>Salary</th>
+        </tr>
+
+    <!-- LOOP FOR SELECT DATA FROM DATABASE -->
+    <?php
+    $sql = "SELECT * FROM customer";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+    ?>
+
+    <tr>
+        <td><?php  echo $row["cus_id"] ?></td>
+        <td><?php  echo $row["cus_name"] ?></td>
+        <td><?php  echo $row["cus_address"] ?></td>
+        <td><?php  echo $row["cus_position"] ?></td>
+        <td><?php  echo $row["cus_salary"] ?></td>
+        </tr>
+
+    <?php
+    }
+    } else {
+    echo "0 results";
+    }
+
+    mysqli_close($conn);
+    ?>
+    </table>
+</div>
